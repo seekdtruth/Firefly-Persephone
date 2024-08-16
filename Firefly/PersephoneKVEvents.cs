53db52c1f312e42239cdcc167a60a8318b9285ae
Darin Loftus (Insight Global LLC) <darin.loftus832@gmail.com>
2024-08-15 18:25:28 -0600

    Create PersephoneKVEvents.cs

---
+++ Firefly/PersephoneKVEvents.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Azure.Messaging.EventHubs;
+
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace Firefly
+{
+    public static class PersephoneKVEvents
+    {
+        [FunctionName("PersephoneKVEvents")]
+        public static async Task Run([EventHubTrigger("persephone", Connection = "EVENT_GRID_CONN_STR")] EventData[] events, ILogger log)
+        {
+            var exceptions = new List<Exception>();
+
+            foreach (EventData eventData in events)
+            {
+                try
+                {
+                    log.LogInformation($"C# Event Hub trigger function processed a message: {eventData.EventBody}");
+                    await Task.Yield();
+                }
+                catch (Exception e)
+                {
+                    // We need to keep processing the rest of the batch - capture this exception and continue.
+                    // Also, consider capturing details of the message that failed processing so it can be processed again later.
+                    log.LogError(e, e.StackTrace);
+                    exceptions.Add(e);
+                }
+            }
+
+            // Once processing of the batch is complete, if any messages in the batch failed processing throw an exception so that there is a record of the failure.
+
+            if (exceptions.Count > 1)
+                throw new AggregateException(exceptions);
+
+            if (exceptions.Count == 1)
+                throw exceptions.Single();
+        }
+    }
+}
