c2cc623685362a1c136d3062cd93e4cd199a39cd
Darin Loftus (Insight Global LLC) <darin.loftus832@gmail.com>
2024-08-21 13:00:22 -0600

    Update UnitTest1.cs

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -1,11 +1,21 @@
-namespace UnitTests
-{
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using UnitTests;
 
+namespace PersephoneAlerting.Tests
+{
+    public class TestingExceptions
+    {
+        [Fact()]
+        public async Task RunTest()
+        {
+            var exception =  await Record.ExceptionAsync(() => PersephoneAlerting.TestingExceptions
+            .RunAsync(
+                new HttpTestRequest(), 
+                new LoggerFactory().CreateLogger("")));
+
+            exception.Should().NotBeNull();
+            exception.Should().BeOfType<DivideByZeroException>();
         }
     }
 }
