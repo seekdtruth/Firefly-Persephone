114ac6456c0961b0fe6c577f67ec4a1333a89a8d
Darin Loftus (Insight Global LLC) <darin.loftus832@gmail.com>
2024-08-23 09:55:09 -0600

    Create GenerateLogs.cs

---
+++ Persephone/GenerateLogs.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace PersephoneAlerting
+{
+    public static class GenerateLogs
+    {
+        [FunctionName("GenerateLogs")]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogDebug("Logging Debug");
+            log.LogMetric("Logging metric", 100);
+            log.LogWarning("Logging warning");
+            log.LogError("Logging error");
+            log.LogCritical("Logging critical");
+
+            return new OkResult();
+        }
+    }
+}
