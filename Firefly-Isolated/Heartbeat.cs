4f6b013ba3bb1ad21ec2f27fb0db23b200a764c2
Darin Loftus (Insight Global LLC) <darin.loftus832@gmail.com>
2024-08-27 14:45:08 -0600

    Create Heartbeat.cs

---
+++ Firefly-Isolated/Heartbeat.cs
@@ -0,0 +1,27 @@
+using System;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Firefly_Isolated
+{
+    public class Heartbeat
+    {
+        private readonly ILogger _logger;
+
+        public Heartbeat(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<Heartbeat>();
+        }
+
+        [Function("Heartbeat")]
+        public void Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer)
+        {
+            _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            
+            if (myTimer.ScheduleStatus is not null)
+            {
+                _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
+            }
+        }
+    }
+}
