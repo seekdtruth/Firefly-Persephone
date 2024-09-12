306c8eec2b29963efb63f7d42acf3bbce8365fb5
seekdtruth <darin.loftus832@gmail.com>
2024-09-12 09:20:58 -0600

    Update StringExtensions.cs

--- Utilities/Extensions/StringExtensions.cs
+++ Utilities/Extensions/StringExtensions.cs
@@ -2,6 +2,13 @@
 {
     public static class StringExtensions
     {
+        public static bool TryParseNullOrEmpty(this string value, out string result)
+        {
+            result = value ?? string.Empty;
+
+            return result.IsNullOrEmpty();
+        }
+
         public static bool IsNullOrWhitespace(this string? value)
         {
             return string.IsNullOrWhiteSpace(value);
