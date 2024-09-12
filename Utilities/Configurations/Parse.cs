c6e310108b29fb603e7f71df35fef8c9043164a4
seekdtruth <darin.loftus832@gmail.com>
2024-09-12 09:21:49 -0600

    Create Parse.cs

---
+++ Utilities/Configurations/Parse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities.Configurations
+{
+    public static class ParseConfigurations
+    {
+        public static ConfigurationSectionType ParseConfigurationSection(string configurationName)
+        {
+            return Enum.TryParse<ConfigurationSectionType>(configurationName, out var section)
+                ? section : throw new ArgumentOutOfRangeException($"{configurationName} is not a valid configuration section");
+        }
+    }
+}
