6d7564f098a41dd5286761017e31b5eb102fe5ae
seekdtruth <darin.loftus832@gmail.com>
2024-09-12 09:21:31 -0600

    Create ConfigurationSectionExtensions.cs

---
+++ Utilities/Configurations/ConfigurationSectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Utilities.Configurations
+{
+    public static class ConfigurationSectionExtensions
+    {
+        public static IConfigurationSection GetSection(this IFireflyConfiguration configuration, ConfigurationSectionType configurationSections)
+        {
+            return configurationSections switch
+            {
+                ConfigurationSectionType.Firefly => configuration.GetSection("Firefly"),
+                ConfigurationSectionType.Persephone => configuration.GetSection("Persephone"),
+                _ => configuration.GetSection("Defaults"),
+            };
+        }
+    }
+}
