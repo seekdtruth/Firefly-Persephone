3291fcc0722feb44ab55fbe69d2786f0e900e662
seekdtruth <darin.loftus832@gmail.com>
2024-09-12 09:10:23 -0600

    Create IFireflyConfiguration.cs

---
+++ Utilities/Configurations/IFireflyConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Azure.Core;
+using Microsoft.Extensions.Configuration;
+
+namespace Utilities.Configurations
+{
+    public interface IFireflyConfiguration : IConfiguration
+    {
+        public IConfigurationSection GetConfigurationSection(ConfigurationSectionType section);
+
+        public string KeyVaultTenantId { get; }
+
+        public string KeyVaultName { get; }
+
+        public Uri KeyVaultUri { get; }
+
+        public TokenCredential KeyVaultCredential { get; }
+    }
+}
