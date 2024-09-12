a3abb24307bb257e7ef3be3ccaed1ec36703a175
seekdtruth <darin.loftus832@gmail.com>
2024-09-12 09:21:17 -0600

    Create FireflyConfigurationBuilder.cs

---
+++ Utilities/Configurations/FireflyConfigurationBuilder.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Utilities.Configurations
+{
+    public class FireflyConfigurationBuilder : ConfigurationBuilder, IFireflyConfigurationBuilder
+    {
+        public FireflyConfigurationBuilder() { }
+
+        public new IConfigurationRoot Build()
+        {
+            return base.Build();
+        }
+    }
+}
