866da5e75ef719d9e89b59c6ad90d246460613e9
seekdtruth <darin.loftus832@gmail.com>
2024-09-25 19:10:39 -0600

    Update StringExtensions.cs

--- Firefly.Utilities/Extensions/StringExtensions.cs
+++ Firefly.Utilities/Extensions/StringExtensions.cs
@@ -6,6 +6,16 @@
     public static class StringExtensions
     {
         /// <summary>
+        /// Checks is a string is null, empty, or whitespace
+        /// </summary>
+        /// <param name="value">The string to check</param>
+        /// <returns>True if the value is null, empty, or whitespace</returns>
+        public static bool IsNullOrWhitespace(this string? value)
+        {
+            return string.IsNullOrWhiteSpace(value);
+        }
+
+        /// <summary>
         /// Checks is a string is null or empty
         /// </summary>
         /// <param name="value">The string to check</param>
