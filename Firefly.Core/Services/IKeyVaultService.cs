496169b699a5425250f84c5e8a97ea070e79adfe
seekdtruth <darin.loftus832@gmail.com>
2024-09-25 18:25:29 -0600

    Create IKeyVaultService.cs

---
+++ Firefly.Core/Services/IKeyVaultService.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Security.KeyVault.Secrets;
+
+namespace Firefly.Core.Services
+{
+    /// <summary>
+    /// Represents an <see cref="IKeyVaultService"/>
+    /// </summary>
+    public interface IKeyVaultService
+    {
+        /// <summary>
+        /// Retrieves a <see cref="KeyVaultSecret"/>
+        /// </summary>
+        /// <param name="secretKey">Name of the <see cref="KeyVaultSecret"/></param>
+        /// <returns>Requested <see cref="KeyVaultSecret"/></returns>
+        KeyVaultSecret GetSecret(string secretKey, CancellationToken cancellationToken = new CancellationToken());
+
+        /// <inheritdoc cref="GetSecret(string, CancellationToken)"/>s
+        Task<KeyVaultSecret> GetSecretAsync(string secretKey, CancellationToken cancellationToken = new CancellationToken());
+
+        /// <summary>
+        /// Retrieves a <see cref="X509Certificate2" />
+        /// </summary>
+        /// <param name="name">Name of certificate to retrieve</param>
+        /// <returns>Requested <see cref="X509Certificate2" /></returns>
+        X509Certificate2 GetCertificate(string name, CancellationToken cancellationToken = new CancellationToken());
+
+        /// <inheritdoc cref="GetCertificate(string, CancellationToken)"/>
+        Task<X509Certificate2> GetCertificateAsnc(string name, CancellationToken cancellationToken = new CancellationToken());
+    }
+}
